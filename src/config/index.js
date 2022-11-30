d730ffb27988b230c3c3edd2b434ffc3676b1d27
alissondel <alisson.delatim@hotmail.com>
2022-11-29 23:55:28 -0300

    first commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
   url: 'https://strapi-v4-test.herokuapp.com/api/pages/?&populate=deep&',
-  siteName: 'Otávio Miranda',
+  siteName: 'Alisson Delatim',
   defaultSlug: 'landing-page',
 };
